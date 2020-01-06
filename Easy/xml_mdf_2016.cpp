6d59cc6dd03296be9c84e74bf8d1cbaadd490ba5
underscoreanuj <underscoreanuj@gmail.com>
2020-01-06 15:27:28 +0530

    plus one

--- Easy/xml_mdf_2016.cpp
+++ Easy/xml_mdf_2016.cpp
@@ -12,51 +12,28 @@ using namespace std;
  * the standard input according to the problem statement.
  **/
 
-double getWeight(string s, int e) {
-    stack<char> ST;
-    for(int i=0; i<e; ++i) {
-        if(s[i] == '-') continue;
-        if(i>0 && s[i-1] == '-' && ST.top() == s[i]) {
-            ST.pop();
-        }
-        else {
-            ST.push(s[i]);
-        }
-    }
-    
-    return 1.0/(1.0 + ST.size());
-}
-
-
-class MySort{
-public:
-    bool operator()(const pair<char, double> o1, const pair<char, double> o2) {
-        if (o1.second > o2.second) return true;
-        else if(o1.second == o2.second) return o1.first < o2.first;
-        return false;
-    }
-};
-
 
 int main()
 {
     string sequence;
     getline(cin, sequence);
 
-    unordered_map<char, double> op;
-    op[sequence[0]] = 1;
+    unordered_map<char, double> res;
+    int depth = 0;
     
-    for(int i=1; i<sequence.length(); ++i) {
-        if(sequence[i] == '-') continue;
-        if(sequence[i-1] != '-') {
-            double tmp = getWeight(sequence, i);
-            op[sequence[i]] += tmp;
+    for(int i=0; i<sequence.length(); ++i) {
+        if(sequence[i] == '-') {
+            res[sequence[++i]] += 1.0/(depth--);
+        }
+        else {
+            ++depth;
         }
     }
     
-    vector<pair<char, double> > result(op.begin(), op.end());
+    auto it = *max_element(res.begin(), res.end(), [](const pair<char, double> &x, const pair<char, double> &y){
+            return x.second < y.second;        
+        });
     
-    sort(result.begin(), result.end(), MySort());
-
-    cout << result[0].first << endl;
+    cout<<it.first<<"\n";
+    
 }
