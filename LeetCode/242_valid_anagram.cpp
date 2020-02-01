f54f85177523b3fd1b647e161be890b69578661c
underscoreanuj <underscoreanuj@gmail.com>
2020-02-01 20:46:31 +0530

    plus one

--- LeetCode/242_valid_anagram.cpp
+++ LeetCode/242_valid_anagram.cpp
@@ -6,19 +6,13 @@ public:
         if (s.size() != t.size())
             return false;
         vector<int> Counts(26, 0);
-        int i = 0;
+
         for (char c : s)
             ++Counts[c - 'a'];
-        for (char s : t)
-        {
-            i = s - 'a';
-            if (Counts[i] == 0)
-                return false;
-            --Counts[i];
-        }
-
+        for (char c : t)
+            --Counts[c - 'a'];
         for (int c : Counts)
-            if (c > 0)
+            if (c != 0)
                 return false;
 
         return true;
