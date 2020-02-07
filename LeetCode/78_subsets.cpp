2e4605b465f39fcca1fd6185350b8ff2c33d9442
underscoreanuj <underscoreanuj@gmail.com>
2020-02-07 21:55:58 +0530

    plus one

--- LeetCode/78_subsets.cpp
+++ LeetCode/78_subsets.cpp
@@ -1,3 +1,25 @@
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         if(nums.size() == 0) return {{}};
+//         vector<vector<int>> ret = {{}};
+//         vector<int> sol;
+//         int sz = 0;
+
+//         for(int n : nums) {
+//             sz = ret.size();
+//             for(int i=0; i<sz; ++i) {
+//                 sol.clear();
+//                 sol = ret[i];
+//                 sol.emplace_back(n);
+//                 ret.emplace_back(sol);
+//             }
+//         }
+
+//         return ret;
+//     }
+// };
+
 class Solution
 {
 public:
