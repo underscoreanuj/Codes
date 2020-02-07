b71988a43cb9858800e7176f48fdc7c29b2f86da
underscoreanuj <underscoreanuj@gmail.com>
2020-02-07 21:34:21 +0530

    plus one

--- LeetCode/74_search_a_2d_matrix.cpp
+++ LeetCode/74_search_a_2d_matrix.cpp
@@ -1,3 +1,37 @@
+// class Solution
+// {
+// public:
+//     bool searchMatrix(vector<vector<int>> &matrix, int target)
+//     {
+//         if (matrix.size() == 0)
+//             return false;
+//         if (matrix[0].size() == 0)
+//             return false;
+//         if (target < matrix[0][0])
+//             return false;
+//         if (target > matrix[matrix.size() - 1][matrix[0].size() - 1])
+//             return false;
+
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         int l = 0, r = m * n - 1, mid = 0, val = 0;
+
+//         while (l <= r)
+//         {
+//             mid = l + ((r - l) >> 1);
+//             val = matrix[mid / n][mid % n];
+//             if (val == target)
+//                 return true;
+//             else if (val < target)
+//                 l = mid + 1;
+//             else
+//                 r = mid - 1;
+//         }
+
+//         return false;
+//     }
+// };
+
 class Solution
 {
 public:
