21ee1192c0d29139747d19ea91f71339e9fa699e
underscoreanuj <underscoreanuj@gmail.com>
2020-02-23 20:08:58 +0530

    plus one

--- LeetCode/238_product_of_array_except_self.cpp
+++ LeetCode/238_product_of_array_except_self.cpp
@@ -25,3 +25,24 @@
 //         return res;
 //     }
 // };
+
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        int tmp = 1;
+        vector<int> res(nums.size(), 1);
+
+        for (int i = 1; i < nums.size(); ++i)
+            res[i] = res[i - 1] * nums[i - 1];
+
+        for (int i = nums.size() - 2; i >= 0; --i)
+        {
+            tmp *= nums[i + 1];
+            res[i] = res[i] * tmp;
+        }
+
+        return res;
+    }
+};
