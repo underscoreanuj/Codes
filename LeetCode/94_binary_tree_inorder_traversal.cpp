6f16a11dcce72da8611ee3121f2413a5d1d1d208
underscoreanuj <underscoreanuj@gmail.com>
2020-03-28 20:50:41 +0530

    plus one

--- LeetCode/94_binary_tree_inorder_traversal.cpp
+++ LeetCode/94_binary_tree_inorder_traversal.cpp
@@ -11,6 +11,77 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// iterative solution - without stack - Morris Traversal
+
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        if (!root)
+            return {};
+
+        vector<int> result;
+        TreeNode *cur = root;
+        TreeNode *tmp = NULL;
+
+        while (cur)
+        {
+            if (cur->left)
+            {
+                tmp = cur->left;
+                while (tmp->right)
+                    tmp = tmp->right;
+                tmp->right = cur;
+                tmp = cur;
+                cur = cur->left;
+                tmp->left = NULL;
+            }
+            else
+            {
+                result.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+
+        return result;
+    }
+};
+
+// iterative solution - using stack
+
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        if (!root)
+            return {};
+
+        vector<int> result;
+        stack<TreeNode *> S;
+        TreeNode *cur = root;
+
+        while (cur || !S.empty())
+        {
+            while (cur)
+            {
+                S.push(cur);
+                cur = cur->left;
+            }
+            cur = S.top();
+            S.pop();
+            result.push_back(cur->val);
+            cur = cur->right;
+        }
+
+        return result;
+    }
+};
+
+// recursive solution
+
 class Solution
 {
 public:
