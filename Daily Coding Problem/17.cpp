af6abd7047f06b3cb789c36ec9226ccd1d7d4902
underscoreanuj <underscoreanuj@gmail.com>
2020-04-04 21:26:46 +0530

    plus one

--- Daily Coding Problem/17.cpp
+++ Daily Coding Problem/17.cpp
@@ -38,3 +38,33 @@
  * 
  * 
 */
+
+class Solution
+{
+public:
+   int lengthLongestPath(string input)
+   {
+      istringstream paths(input);
+      string path;
+      int max_path_len = 0, depth = 0;
+
+      unordered_map<int, int> depth_map;
+
+      while (getline(paths, path))
+      {
+         depth = path.find_last_of("\t");
+         depth = (depth == string::npos) ? 0 : depth + 1;
+
+         if (path.find(".") != string::npos)
+         { // it is a file
+            max_path_len = max(max_path_len, int(depth_map[depth] + path.length()));
+         }
+         else
+         {
+            depth_map[depth + 1] = depth_map[depth] + path.length() - depth;
+         }
+      }
+
+      return max_path_len;
+   }
+};
