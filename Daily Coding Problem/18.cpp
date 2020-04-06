7d8af5f63aa9d2233fcd4db4270fc2cac5eab7db
underscoreanuj <underscoreanuj@gmail.com>
2020-04-06 11:57:07 +0530

    plus one

--- Daily Coding Problem/18.cpp
+++ Daily Coding Problem/18.cpp
@@ -13,3 +13,26 @@
  * Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
  * 
 */
+
+class Solution
+{
+public:
+   vector<int> maxSlidingWindow(vector<int> &nums, int k)
+   {
+      vector<int> result;
+      deque<int> window;
+
+      for (int i = 0; i < nums.size(); ++i)
+      {
+         while (!window.empty() && nums[i] >= nums[window.back()])
+            window.pop_back();
+         window.emplace_back(i);
+         if (i >= k - 1)
+            result.emplace_back(nums[window.front()]);
+         while (!window.empty() && window.front() <= i - k + 1)
+            window.pop_front();
+      }
+
+      return result;
+   }
+};
