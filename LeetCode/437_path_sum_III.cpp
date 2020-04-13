a56a9bc5a08f21a20f6e9d78a7b818857a473234
underscoreanuj <underscoreanuj@gmail.com>
2020-04-13 09:17:37 +0530

    plus one

--- LeetCode/437_path_sum_III.cpp
+++ LeetCode/437_path_sum_III.cpp
@@ -1,6 +1,52 @@
-// O(n) solution
+// O(n) time solution and O(logn) space
 
-// O(n^2) solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    int result;
+    unordered_map<int, int> MEM;
+
+    void pathSumUtil(TreeNode *root, int pre_sum, int sum)
+    {
+        if (!root)
+            return;
+
+        pre_sum += root->val;
+        if (MEM.count(pre_sum - sum))
+        {
+            result += MEM[pre_sum - sum];
+        }
+
+        ++MEM[pre_sum];
+
+        pathSumUtil(root->left, pre_sum, sum);
+        pathSumUtil(root->right, pre_sum, sum);
+
+        --MEM[pre_sum];
+    }
+
+    int pathSum(TreeNode *root, int sum)
+    {
+        result = 0;
+        int pre_sum = 0;
+        MEM[pre_sum] = 1;
+
+        pathSumUtil(root, pre_sum, sum);
+
+        return result;
+    }
+};
+
+// O(n^2) solution and O(logn) space (stack space of recursion)
 
 /**
  * Definition for a binary tree node.
