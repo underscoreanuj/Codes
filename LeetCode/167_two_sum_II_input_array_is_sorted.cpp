7c3754eb59b58420b129402eb01fa28833d59e57
underscoreanuj <underscoreanuj@gmail.com>
2020-04-29 22:45:23 +0530

    plus one

---
+++ LeetCode/167_two_sum_II_input_array_is_sorted.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r)
+        {
+            if (numbers[l] + numbers[r] == target)
+                return {l + 1, r + 1};
+            else if (numbers[l] + numbers[r] < target)
+                ++l;
+            else
+                --r;
+        }
+        return {};
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
