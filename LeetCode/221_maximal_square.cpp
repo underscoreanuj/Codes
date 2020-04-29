20c5162e1a559bf03d87225977cd0c4bc669d66a
underscoreanuj <underscoreanuj@gmail.com>
2020-04-29 20:23:55 +0530

    plus one

--- LeetCode/221_maximal_square.cpp
+++ LeetCode/221_maximal_square.cpp
@@ -1,3 +1,90 @@
+// space efficient dp solution : O(m*n) time, O(n) space
+
+class Solution
+{
+public:
+    int maximalSquare(vector<vector<char>> &matrix)
+    {
+        if (matrix.empty())
+            return 0;
+
+        int m = matrix.size(), n = matrix[0].size(), result = 0, prev = 0, cur = 0;
+        int tab[n];
+        memset(tab, 0, sizeof(tab));
+
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                cur = tab[j];
+                if (matrix[i][j] == '1')
+                {
+                    if (i == 0 || j == 0)
+                        tab[j] = 1;
+                    else
+                        tab[j] = min(prev, min(tab[j], tab[j - 1])) + 1;
+                    result = max(result, tab[j]);
+                }
+                else
+                    tab[j] = 0;
+                prev = cur;
+            }
+        }
+
+        return result * result;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+// dp solution : O(m*n) time, O(m*n) space
+
+class Solution
+{
+public:
+    int maximalSquare(vector<vector<char>> &matrix)
+    {
+        if (matrix.empty())
+            return 0;
+
+        int m = matrix.size(), n = matrix[0].size(), result = 0;
+        int tab[m][n];
+
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (matrix[i][j] == '1')
+                {
+                    if (i == 0 || j == 0)
+                        tab[i][j] = 1;
+                    else
+                        tab[i][j] = min(tab[i - 1][j - 1], min(tab[i - 1][j], tab[i][j - 1])) + 1;
+                    result = max(result, tab[i][j]);
+                }
+                else
+                    tab[i][j] = 0;
+            }
+        }
+
+        return result * result;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+// brute force : O((m*n)^2) time, O(1) space
+
 class Solution
 {
 public:
