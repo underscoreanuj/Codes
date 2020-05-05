b342bd3c4ff74c10376f94ad285215c17903f16e
underscoreanuj <underscoreanuj@gmail.com>
2020-05-05 17:45:59 +0530

    plus one

---
+++ Hackerearth/Algorithms/Searching/Binary Search/Bugs.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main()
+{
+    int n = 0, c = 0, p = 0;
+    cin >> n;
+    vector<int> a;
+
+    while (n--)
+    {
+        cin >> c;
+        if (c == 1)
+        {
+            cin >> p;
+            a.push_back(p);
+        }
+        else
+        {
+            if (a.size() < 3)
+                cout << "Not enough enemies\n";
+            else
+            {
+                nth_element(a.begin(), a.begin() + ((a.size() / 3) - 1), a.end(), greater<int>());
+                cout << a[(a.size() / 3) - 1] << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
