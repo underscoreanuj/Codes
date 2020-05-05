1e31b4263f1a85d7bb0c6ef341687c348c23cc59
underscoreanuj <underscoreanuj@gmail.com>
2020-05-05 17:53:04 +0530

    plus one

--- Hackerearth/Algorithms/Searching/Binary Search/Bugs.cpp
+++ Hackerearth/Algorithms/Searching/Binary Search/Bugs.cpp
@@ -21,7 +21,7 @@ int main()
         if (c == 1)
         {
             cin >> p;
-            a.push_back(p);
+            a.insert(upper_bound(a.begin(), a.end(), p), p);
         }
         else
         {
@@ -29,11 +29,50 @@ int main()
                 cout << "Not enough enemies\n";
             else
             {
-                nth_element(a.begin(), a.begin() + ((a.size() / 3) - 1), a.end(), greater<int>());
-                cout << a[(a.size() / 3) - 1] << "\n";
+                cout << a[a.size() - (a.size() / 3)] << "\n";
             }
         }
     }
 
     return 0;
 }
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// auto speedup = []() {
+//     std::ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     return nullptr;
+// }();
+
+// int main()
+// {
+//     int n = 0, c = 0, p = 0;
+//     cin >> n;
+//     vector<int> a;
+
+//     while (n--)
+//     {
+//         cin >> c;
+//         if (c == 1)
+//         {
+//             cin >> p;
+//             a.push_back(p);
+//         }
+//         else
+//         {
+//             if (a.size() < 3)
+//                 cout << "Not enough enemies\n";
+//             else
+//             {
+//                 nth_element(a.begin(), a.begin() + ((a.size() / 3) - 1), a.end(), greater<int>());
+//                 cout << a[(a.size() / 3) - 1] << "\n";
+//             }
+//         }
+//     }
+
+//     return 0;
+// }
