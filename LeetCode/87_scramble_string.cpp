240e6bdb24dedb54b3ed78e437b0463f6a2c2621
underscoreanuj <underscoreanuj@gmail.com>
2020-05-12 14:17:47 +0530

    plus one

--- LeetCode/87_scramble_string.cpp
+++ LeetCode/87_scramble_string.cpp
@@ -1,3 +1,53 @@
+// efficient recursive solution with memoization
+
+class Solution
+{
+public:
+    vector<vector<vector<int>>> MEM;
+
+    bool isScrambleUtil(string &s1, string &s2, int i1, int i2, int len)
+    {
+        if (len == 1)
+            return s1[i1] == s2[i2];
+        if (MEM[i1][i2][len] != -1)
+            return MEM[i1][i2][len];
+
+        for (int i = 1; i < len; ++i)
+        {
+            if (isScrambleUtil(s1, s2, i1, i2, i) && isScrambleUtil(s1, s2, i1 + i, i2 + i, len - i))
+            {
+                MEM[i1][i2][len] = 1;
+                return true;
+            }
+            if (isScrambleUtil(s1, s2, i1, i2 + len - i, i) && isScrambleUtil(s1, s2, i1 + i, i2, len - i))
+            {
+                MEM[i1][i2][len] = 1;
+                return true;
+            }
+        }
+
+        MEM[i1][i2][len] = 0;
+        return false;
+    }
+
+    bool isScramble(string s1, string s2)
+    {
+        if (s1.length() != s2.length())
+            return false;
+
+        MEM = vector<vector<vector<int>>>(s1.length(), vector<vector<int>>(s1.length(), vector<int>(s1.length() + 1, -1)));
+
+        return isScrambleUtil(s1, s2, 0, 0, s1.length());
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
 // recursive solution with memoization
 
 class Solution
@@ -41,6 +91,55 @@ public:
         unordered_map<string, bool> MEM;
 
         return isScrambleUtil(s1, s2, MEM);
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+// dp solution
+
+class Solution
+{
+public:
+    bool isScramble(string s1, string s2)
+    {
+        if (s1.length() != s2.length())
+            return false;
+
+        int len = s1.length();
+        vector<vector<vector<bool>>> MEM(len, vector<vector<bool>>(len, vector<bool>(len + 1, false)));
+
+        for (int k = 1; k <= len; ++k)
+        {
+            for (int i = len - k; i >= 0; --i)
+            {
+                for (int j = len - k; j >= 0; --j)
+                {
+                    if (k == 1)
+                    {
+                        MEM[i][j][k] = s1[i] == s2[j];
+                    }
+                    else
+                    {
+                        for (int q = 1; q < k; ++q)
+                        {
+                            if ((MEM[i][j][q] && MEM[i + q][j + q][k - q]) || (MEM[i][j + k - q][q] && MEM[i + q][j][k - q]))
+                            {
+                                MEM[i][j][k] = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return MEM[0][0][len];
     }
 };
 
