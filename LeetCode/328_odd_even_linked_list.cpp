63f7cf9dba521c1935c9de96b0f2b4d5fdfe991f
underscoreanuj <underscoreanuj@gmail.com>
2020-05-16 17:45:28 +0530

    plus one

---
+++ LeetCode/328_odd_even_linked_list.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *oddEvenList(ListNode *head)
+    {
+        if (!head)
+            return NULL;
+
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *even_head = even;
+
+        while (odd && even && odd->next && even->next)
+        {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+
+        odd->next = even_head;
+
+        return head;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
