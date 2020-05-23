0f9d2e09672a1af297ced4a592ab9d5c92d0a7b2
underscoreanuj <underscoreanuj@gmail.com>
2020-05-23 14:35:48 +0530

    plus one

--- LeetCode/65_valid_number.cpp
+++ LeetCode/65_valid_number.cpp
@@ -8,12 +8,13 @@ public:
         int i = 0;
         while (i < s.length() && s[i] == ' ')
             ++i;
+
         s = s.substr(i);
 
         i = s.length() - 1;
-
         while (i >= 0 && s[i] == ' ')
             --i;
+
         s = s.substr(0, i + 1);
     }
 
@@ -21,22 +22,22 @@ public:
     {
         trim(s);
 
-        bool num_found = false, decimal_found = false, exponent_found = false;
+        bool num_found = false, exponent_found = false, decimal_found = false;
 
         for (int i = 0; i < s.length(); ++i)
         {
             switch (s[i])
             {
+            case '.':
+                if (decimal_found || exponent_found)
+                    return false;
+                decimal_found = true;
+                break;
             case 'e':
                 if (exponent_found || !num_found)
                     return false;
                 exponent_found = true;
                 num_found = false;
-                break;
-            case '.':
-                if (decimal_found || exponent_found)
-                    return false;
-                decimal_found = true;
                 break;
             case '+':
             case '-':
@@ -52,6 +53,78 @@ public:
         }
 
         return num_found;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+// automaton solution
+
+class Solution
+{
+public:
+    bool isNumber(string s)
+    {
+        string BLANK = "blank";
+        string SIGN = "sign";
+        string NUMBER = "number";
+        string DECIMAL = "decimal";
+        string EXPONENT = "exponent";
+
+        vector<unordered_map<string, int>> automaton = {
+            // *** => final state
+            {},
+            // state 1 : starting state                 | expects : blank, sign, number, decimal
+            {{BLANK, 1}, {SIGN, 2}, {NUMBER, 3}, {DECIMAL, 4}},
+            // state 2 : sign                           | expects : number, decimal
+            {{DECIMAL, 4}, {NUMBER, 3}},
+            // ***state 3 : number                      | expects : blank, number, decimal, exponent
+            {{BLANK, 9}, {NUMBER, 3}, {DECIMAL, 5}, {EXPONENT, 6}},
+            // state 4 : decimal                        | expects : number
+            {{NUMBER, 5}},
+            // ***state 5 : after decimal               | expects :  blank, number, exponent
+            {{BLANK, 9}, {NUMBER, 5}, {EXPONENT, 6}},
+            // state 6 : exponent                       | expects : sign, number
+            {{SIGN, 7}, {NUMBER, 8}},
+            // state 7 : sign after exponent            | expects : number
+            {{NUMBER, 8}},
+            // ***state 8 : number after exponent/sign  | expects : blank, number
+            {{BLANK, 9}, {NUMBER, 8}},
+            // ***state 9 : blank                       | expects : blank
+            {{BLANK, 9}}};
+
+        int cur_state = 1;
+        string state = "";
+
+        for (char &c : s)
+        {
+            if (c == ' ')
+                state = BLANK;
+            else if (c == '+' || c == '-')
+                state = SIGN;
+            else if (c == '.')
+                state = DECIMAL;
+            else if (c == 'e')
+                state = EXPONENT;
+            else if (isdigit(c))
+                state = NUMBER;
+            else
+                return false;
+
+            if (automaton[cur_state].count(state))
+            {
+                cur_state = automaton[cur_state][state];
+            }
+            else
+                return false;
+        }
+
+        return (cur_state == 3 || cur_state == 5 || cur_state == 8 || cur_state == 9);
     }
 };
 
