62586ba8d65be64a9465e31b0d814749206f18bd
underscoreanuj <underscoreanuj@gmail.com>
2020-05-31 20:26:02 +0530

    plus one

---
+++ LeetCode/111_minimum_depth_of_binary_tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int minDepth(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+
+        int l = minDepth(root->left);
+        int r = minDepth(root->right);
+        return 1 + ((l == 0 || r == 0) ? max(l, r) : min(l, r));
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
