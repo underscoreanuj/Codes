a8e5492514bdceaf7d45478d133e3ef76ffa19fc
underscoreanuj <underscoreanuj@gmail.com>
2020-06-16 10:15:24 +0530

    plus one

---
+++ LeetCode/701_insert_into_a_binary_searchTree.cpp
@@ -0,0 +1,92 @@
+// recursive solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *insertIntoBST(TreeNode *root, int val)
+    {
+        if (!root)
+            return new TreeNode(val);
+
+        if (root->val > val)
+            root->left = insertIntoBST(root->left, val);
+        else
+            root->right = insertIntoBST(root->right, val);
+        return root;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+// iterative solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *insertIntoBST(TreeNode *root, int val)
+    {
+        TreeNode *cur = root;
+        TreeNode *new_node = new TreeNode(val);
+
+        while (cur)
+        {
+            if (cur->val > val)
+            { // go left
+                if (cur->left)
+                    cur = cur->left;
+                else
+                {
+                    cur->left = new_node;
+                    return root;
+                }
+            }
+            else
+            { // go right
+                if (cur->right)
+                    cur = cur->right;
+                else
+                {
+                    cur->right = new_node;
+                    return root;
+                }
+            }
+        }
+
+        // root is null
+        return new_node;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
