7e4961e6be8613a10bec6a4c59c4249c087f5e6f
underscoreanuj <underscoreanuj@gmail.com>
2020-09-05 16:08:23 +0530

    plus one

--- LeetCode/275_h-index_II.cpp
+++ LeetCode/275_h-index_II.cpp
@@ -1,3 +1,36 @@
+// O(logn) solution
+
+class Solution
+{
+public:
+    int hIndex(vector<int> &citations)
+    {
+        int n = citations.size();
+        int l = 0, r = n - 1, mid = 0;
+
+        while (l <= r)
+        {
+            mid = l + ((r - l) >> 1);
+
+            if (citations[mid] >= n - mid)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+
+        return n - l;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+// O(n) solution
+
 class Solution
 {
 public:
