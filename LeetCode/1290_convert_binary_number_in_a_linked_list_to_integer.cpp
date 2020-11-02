1f3674f42866a49bcdbfbbcd740e8ae35a1aaa7d
underscoreanuj <underscoreanuj@gmail.com>
2020-11-02 13:22:30 +0530

    plus on

---
+++ LeetCode/1290_convert_binary_number_in_a_linked_list_to_integer.cpp
@@ -0,0 +1,73 @@
+// bit manipulation approach
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    int getDecimalValue(ListNode *head)
+    {
+        int result = 0;
+        ListNode *cur = head;
+
+        while (cur)
+        {
+            result = result << 1 | cur->val;
+            cur = cur->next;
+        }
+
+        return result;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+// standard approach
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    int getDecimalValue(ListNode *head)
+    {
+        int result = head->val;
+        ListNode *cur = head;
+
+        while (cur->next)
+        {
+            cur = cur->next;
+            result = result * 2 + cur->val;
+        }
+
+        return result;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
