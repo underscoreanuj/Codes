dc57feeff967f3345dc9b6e101abe22ce6981cbd
underscoreanuj <underscoreanuj@gmail.com>
2021-01-06 13:37:18 +0530

    plus one

---
+++ LeetCode/1534_count_good_triplets.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int countGoodTriplets(vector<int> &arr, int a, int b, int c)
+    {
+        int n = arr.size(), result = 0;
+
+        for (int i = 0; i < n - 2; ++i)
+            for (int j = i + 1; j < n - 1; ++j)
+                for (int k = j + 1; k < n; ++k)
+                    if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
+                        ++result;
+
+        return result;
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
